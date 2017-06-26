22bacbc4f3350b7501b844feb80f408f1bf35003
yicheng <963364424@qq.com>
2017-06-26 21:00:41 +0800

    增加后台自动更新功能

--- app/src/main/java/com/example/fiver/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/fiver/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.fiver.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.fiver.coolweather.gson.ForeCast;
 import com.example.fiver.coolweather.gson.Weather;
+import com.example.fiver.coolweather.service.AutoUpdateService;
 import com.example.fiver.coolweather.util.HttpUtil;
 import com.example.fiver.coolweather.util.Utility;
 
@@ -181,6 +183,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
                         }else{
                             Toast.makeText(WeatherActivity.this,"获取天气失败",Toast.LENGTH_SHORT).show();
                         }
